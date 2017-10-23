c5e05691c1dec2aa542d85d03bb19e53a5480400
rrybalov2 <rrybalov@andrew.cmu.edu>
2017-10-22 22:27:12 -0400

    Ross Rybalov FINAL 10.22 10:27PM
    Final Submission

--- cartjs.js
+++ cartjs.js
@@ -1,3 +1,4 @@
+/* Object Constructor*/
 function cartItem(packageType, amount, flavor1, flavor2, price){
     this.packageType=packageType;
     this.amount=amount;
@@ -9,43 +10,49 @@ function cartItem(packageType, amount, flavor1, flavor2, price){
 /* Starting a JQuery document ready function to make sure the DOM is ready for JS */
 $( document ).ready(function() {
     console.log( "ready!" );
-
+    //Initializing number of items in cart & setting to 0
     var numForCart = 0;
+    //Grabbing current number of items in cart from local storage
     document.getElementById("numForCart").innerHTML = JSON.parse(localStorage.getItem("numForCart"));
-
+    //Grabbing array of items from product page
     var dataFromCart = JSON.parse(localStorage.getItem("thingsInCartArray"));
     console.log(dataFromCart);
 
-    if (dataFromCart == null) {}
-
+    /* Creating an if/else statement to display nothing or pass array length into variable */
+    if (dataFromCart == null) {
+    }
     else {
         var arrayLength = dataFromCart.length;
     }
-
+    /* For loop that runs the length of array to pull all data  */
     for (var i = 0; i < arrayLength; i++) {
+        //Adding table row with all data from array with the name that includes the position within array
         $("#cartTable").append('<tr id=item' + [i] +'></tr>');
+        //Adding in each individual data point & placing into separate cell
         $("#item" + [i]).append('<td>' + dataFromCart[i].amount + "  servings" + "  of  " + '</td>' + '<td>' + dataFromCart[i].packageType + '</td>' +
             '<td>' + dataFromCart[i].flavor1 + '</td>' + '<td>' + dataFromCart[i].flavor2 + '</td>' + '<td>' + " $" + dataFromCart[i].price) + '</td>';
+        //Adding a remove button at the end
         $("#item" + [i]).append('<button class=removeButton id=' + [i] + '>Remove Item</button>');
         console.log("#item" + [i]);
-        }
-
+    }
+    /* Creating a remove button - Mary Beth helped with this one as well */
     $('.removeButton').click(function(){
+        //Initializing variable that grabs previous position
         var itemPosition = $(this).attr('id');
+        //Initializing variable that grabs amount to remove from cart
         var numToRemove = dataFromCart[itemPosition].amount;
+        //Grabbing current cart value
         var currCartNum = JSON.parse(localStorage.getItem("numForCart"));
+        //Subtracting amount to remove from current cart amount
         currCartNum = currCartNum - numToRemove;
         console.log("cart info new: " + currCartNum);
+        //Storing it back into local storage
         localStorage.setItem("numForCart", JSON.stringify(currCartNum));
         document.getElementById("numForCart").innerHTML = JSON.parse(localStorage.getItem("numForCart"));
-
+        //Removing cart item from local storage
         $("#item"+itemPosition).remove();
         dataFromCart.splice(itemPosition, 1);
         localStorage.setItem("thingsInCartArray", JSON.stringify(dataFromCart));
-            });
-
-/* End of doc ready */
+    });
+    /* End of doc ready */
 });
-
-
-
--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-/*
-function changeMainPhoto() {
-    var bunType = document.getElementById("bun-pack-type").value;
-
-    if (bunType == "single") {
-        document.getElementById("mainBun").src="./Assets/baconbun.jpg";
-    }
-    else if (bunType == "sixpack") {
-        document.getElementById("mainBun").src="./Assets/blackberrybun.jpg";
-    }
-    else if (bunType == "dozen") {
-        document.getElementById("mainBun").src="./Assets/birthdaycake.jpg";
-    }
-} */
-
-
+/* Object Constructor*/
 function cartItem(packageType, amount, flavor1, flavor2, price){
     this.packageType=packageType;
     this.amount=amount;
@@ -22,42 +7,37 @@ function cartItem(packageType, amount, flavor1, flavor2, price){
     this.price=price;
 }
 
+/* Initializing and setting global variable current price of all items to 1 ($1) for now */
 var currPrice = 1;
 
 /* Starting a JQuery document ready function to make sure the DOM is ready for JS */
 $( document ).ready(function() {
     console.log( "ready!" );
 
+    //Passing numForCart to local storage
     document.getElementById("numForCart").innerHTML = JSON.parse(localStorage.getItem("numForCart"));
-
-/*
-document.getElementById("bun-pack-type").onchange = function() {
-    if (document.getElementById("bun-pack-type").value === "sixpack") {
-        document.getElementById("mainBun").src="./Assets/baconbun.jpg";
-    }
-
-
-}
-*/
-
-//function
-
+    //Passing cart items into new variable
     var dataFromCart = JSON.parse(localStorage.getItem("thingsInCartArray"));
     console.log(dataFromCart);
 
-/* Using a switch statement to bundle actions together */
-document.getElementById("bun-pack-type").onchange = function(){
-    switch(document.getElementById("bun-pack-type").value) {
+    /* Using a switch statement to bundle actions together */
+    document.getElementById("bun-pack-type").onchange = function(){
+        switch(document.getElementById("bun-pack-type").value) {
         //Dropdown option: Six Pack of Original Buns
         case "Six Pack":
             //image from Tesco: https://img.tesco.com/Groceries/pi/000/0286020000000/IDShot_540x540.jpg
             document.getElementById("mainBun").src="./Assets/sixpackrolls.jpg";
+            //Price text
             document.getElementById("showP").innerHTML = "$5 per six pack";
+            //Description text
             document.getElementById("showDesc").innerHTML = "Get them by the six pack!";
+            //First flavor dropdown
             document.getElementById("first-flavor").style.display='block';
+            //Second flavor dropdown
             document.getElementById("second-flavor").style.display='block';
+            //using global variable for prive value to later pass into cart
             currPrice = 5;
-        break;
+            break;
 
         //Dropdown option: Single Bun
         case "Single":
@@ -67,7 +47,7 @@ document.getElementById("bun-pack-type").onchange = function(){
             document.getElementById("first-flavor").style.display='none';
             document.getElementById("second-flavor").style.display='none';
             currPrice = 1;
-        break;
+            break;
 
         //Dropdown option: Dozen Original Buns
         case "Dozen Pack":
@@ -78,7 +58,7 @@ document.getElementById("bun-pack-type").onchange = function(){
             document.getElementById("first-flavor").style.display='block';
             document.getElementById("second-flavor").style.display='block';
             currPrice = 11;
-        break;
+            break;
 
         //Dropdown option: Six Pack of Mixed Buns
         case "Six Pack Mixed Flavors":
@@ -89,7 +69,7 @@ document.getElementById("bun-pack-type").onchange = function(){
             document.getElementById("first-flavor").style.display='block';
             document.getElementById("second-flavor").style.display='block';
             currPrice = 5;
-        break;
+            break;
 
         //Dropdown option: Dozen of Mixed Buns
         case "Dozen Pack Mixed Flavors":
@@ -100,57 +80,64 @@ document.getElementById("bun-pack-type").onchange = function(){
             document.getElementById("first-flavor").style.display='block';
             document.getElementById("second-flavor").style.display='block';
             currPrice = 11;
-        break;
-    }
-}
-
-/* Manipulating number at the top of the cart */
-//cart # - change variables
-var numForCart = 0;
-// add is my button
-document.getElementById("add").addEventListener("click", addCart)
-function addCart() {
-    console.log( "function ready!" );
-    // Mary Beth helped with the suntax for += +
-    numForCart += + document.getElementById("amount-select").value;
-    console.log(numForCart)
-    localStorage.setItem("numForCart", JSON.stringify(numForCart));
-    document.getElementById("numForCart").innerHTML = JSON.parse(localStorage.getItem("numForCart"));
-}
-
-/* */
-document.getElementById("add").addEventListener("click", storeItem)
-function storeItem() {
-    var chosenPackageType = document.getElementById("bun-pack-type");
-    var chosenPackageTypeValue = chosenPackageType.options[chosenPackageType.selectedIndex].value;
-    var chosenAmount = document.getElementById("amount-select");
-    var chosenAmountValue = chosenAmount.options[chosenAmount.selectedIndex].value;
-    var chosenPriceValue = chosenAmountValue * currPrice;
-    console.log("print num:" + chosenAmountValue + "print price:" + chosenPriceValue);
-
-    if (chosenPackageTypeValue == "single") {
-        var chosenFirstFlavorValue = " ";
-        var chosenSecondFlavorValue = " ";
+            break;
+        }
     }
 
-    else {
-    var chosenFirstFlavor = document.getElementById("dropdown1");
-    var chosenFirstFlavorValue = chosenFirstFlavor.options[chosenFirstFlavor.selectedIndex].value;
-    var chosenSecondFlavor = document.getElementById("dropdown2");
-    var chosenSecondFlavorValue = chosenSecondFlavor.options[chosenSecondFlavor.selectedIndex].value;
+    /* Manipulating number at the top of the page by the cart icon */
+    var numForCart = 0;
+    //creating click event -- add is my button
+    document.getElementById("add").addEventListener("click", addCart)
+    //Function that grabs the amount of product from the page and adds it to the cart counter
+    function addCart() {
+        console.log( "function ready!" );
+        //Mary Beth helped with the shorthand syntax for += +
+        numForCart += + document.getElementById("amount-select").value;
+        console.log(numForCart)
+        localStorage.setItem("numForCart", JSON.stringify(numForCart));
+        document.getElementById("numForCart").innerHTML = JSON.parse(localStorage.getItem("numForCart"));
     }
 
+    /* Passing selected items into local storage*/
+    document.getElementById("add").addEventListener("click", storeItem)
+    //Function that passes each dropdown value into local storage
+    function storeItem() {
+        //Get package type
+        var chosenPackageType = document.getElementById("bun-pack-type");
+        //Pass value to variable
+        var chosenPackageTypeValue = chosenPackageType.options[chosenPackageType.selectedIndex].value;
+        //Get quantity amount
+        var chosenAmount = document.getElementById("amount-select");
+        //Pass value to variable
+        var chosenAmountValue = chosenAmount.options[chosenAmount.selectedIndex].value;
+        //Create variable that multiplies amount of items times their price for accurate cart pricing
+        var chosenPriceValue = chosenAmountValue * currPrice;
+        console.log("print num:" + chosenAmountValue + "print price:" + chosenPriceValue);
+        //IF statement that makes first & second flavor options invisible in the cart
+        if (chosenPackageTypeValue == "single") {
+            var chosenFirstFlavorValue = " ";
+            var chosenSecondFlavorValue = " ";
+        }
+        else {
+        //Get first flavor
+        var chosenFirstFlavor = document.getElementById("dropdown1");
+        //Pass value to variable
+        var chosenFirstFlavorValue = chosenFirstFlavor.options[chosenFirstFlavor.selectedIndex].value;
+        //Get second flavor
+        var chosenSecondFlavor = document.getElementById("dropdown2");
+        //Pass value to variable
+        var chosenSecondFlavorValue = chosenSecondFlavor.options[chosenSecondFlavor.selectedIndex].value;
+    }
 
-
+    //Pass all variables into array
     var userChoice = new cartItem(chosenPackageTypeValue, chosenAmountValue, chosenFirstFlavorValue, chosenSecondFlavorValue, chosenPriceValue);
-
+    //Adding array into local storage
     var thingsInCart = JSON.parse(localStorage.getItem("thingsInCartArray")) || [];
     thingsInCart.push(userChoice);
-
+    //Converting to strings
     localStorage.setItem("thingsInCartArray", JSON.stringify(thingsInCart));
     console.log(thingsInCart);
-
-}
+    }
 
 /* End of doc ready */
 });
